81ccfc50f144846b95b58d2608538d4f4e629ae4
HiIamClayton <claytonadonis225@gmail.com>
2023-12-19 19:56:30 +0200

    Navbar Skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <template>
-<h1>Bootstrapped in</h1>
+<div id="navbar" class="container">
+<div class="row">
+  <div class="col" id="home"><a href="#home" class="links">Home</a></div>
+  <div class="col"><a href="" class="links">About me</a></div>
+  <div class="col"><a href="" class="links">Testimonials</a></div>
+  <div class="col"><a href="" class="links">Projects</a></div>
+  <div class="col"><a href="" class="links">Contact Me</a></div>
+</div>
+</div>
 </template>
 
 <style>
 * {
   margin: 0;
   padding: 0;
+}
+
+#navbar {
+  margin: 0;
+  width: 30vw;
+  background-color: aqua;
+  text-align: center;
+  border-bottom-right-radius: 10px;
+  position: fixed;
+}
+
+a:link {
+  text-decoration: none;
+}
+
+.links {
+  color: brown;
+  font-size: 1.5vh;
 }
 </style>
